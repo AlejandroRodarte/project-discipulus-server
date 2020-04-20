4f4a3415e16a5e9bd93efaf67932fda13ddc8c76
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-20 15:09:33 -0600

    Add rest of attributes for user schema (except file schema)

--- src/db/schemas/user.js
+++ src/db/schemas/user.js
@@ -1,4 +1,5 @@
 const { Schema } = require('mongoose');
+const validator = require('validator').default;
 
 const regexp = require('../../util/regexp');
 const badWordsFilter = require('../../util/filter/bad-words-filter');
@@ -28,7 +29,54 @@ const userSchema = new Schema({
         minlength: 3,
         maxlength: 100,
         trim: false
-    }
+    },
+
+    username: {
+        type: String,
+        required: true,
+        validate: [
+            (value) => {
+
+                if (!regexp.username.test(value)){
+                    return false;
+                }
+
+                if (badWordsFilter.isProfane(value)) {
+                    return false;
+                }
+
+                return true;
+
+            },
+            'Please provide a valid name (no bad words!)'
+        ],
+        unique: true,
+        minlength: 4,
+        maxlength: 20,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        validate: [
+            (value) => validator.isEmail(value),
+            'Please provide a valid email'
+        ],
+        unique: true,
+        trim: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minlength: 60,
+        maxlength: 60
+    },
+
+    tokens: [{
+        type: String
+    }]
 
 });
 
--- src/util/regexp.js
+++ src/util/regexp.js
@@ -1,5 +1,6 @@
 module.exports = {
     roleName: /^ROLE_/,
     singleName: /^\p{L}+$/,
-    fullName: /^(?:[\p{L}\p{Mn}\p{Pd}\'\x{2019}]+\s[\p{L}\p{Mn}\p{Pd}\'\x{2019}]+\s?)+$/
+    fullName: /^(?:[\p{L}\p{Mn}\p{Pd}\'\x{2019}]+\s[\p{L}\p{Mn}\p{Pd}\'\x{2019}]+\s?)+$/,
+    username: /^(?![_.\d])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/
 };
