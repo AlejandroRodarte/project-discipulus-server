bfe3c056117a51849de50521689d2ae87077f9b1
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-20 14:07:25 -0600

    Write basic tests for invalid role names

--- test/db/models/role.test.js
+++ test/db/models/role.test.js
@@ -13,4 +13,46 @@ describe('Invalid role models', () => {
 
     });
 
+    test('Should not create a role that does not start with ROLE_', () => {
+
+        const roleDoc = {
+            name: 'MY_USER_ROLE'
+        };
+
+        const role = new Role(roleDoc);
+
+        const validationError = role.validateSync();
+
+        expect(validationError).toBeDefined();
+
+    });
+
+    test('Should not create a role shorter than 6 characters', () => {
+
+        const roleDoc = {
+            name: 'ROLE_'
+        };
+
+        const role = new Role(roleDoc);
+
+        const validationError = role.validateSync();
+
+        expect(validationError).toBeDefined();
+
+    });
+
+    test('Should not create a role longer than 30 characters', () => {
+
+        const roleDoc = {
+            name: 'ROLE_SUPER_ADMINISTRATOR_MAGICIAN_OWNER'
+        };
+
+        const role = new Role(roleDoc);
+
+        const validationError = role.validateSync();
+
+        expect(validationError).toBeDefined();
+
+    });
+
 });
