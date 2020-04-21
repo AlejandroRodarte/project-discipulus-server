e5f02d5ced08ec79ca95b8c7380e027326e8801f
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-20 19:37:24 -0600

    Add custom validation messages to role schema

--- src/db/schemas/role.js
+++ src/db/schemas/role.js
@@ -5,11 +5,11 @@ const regexp = require('../../util/regexp');
 const roleSchema = new Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, 'A role name is required'],
         validate: [regexp.roleName, 'Roles must start with the ROLE_ keyword'],
         unique: true,
-        minlength: 6,
-        maxlength: 30,
+        minlength: [6, 'Role name must be at least 6 characters long'],
+        maxlength: [30, 'Role name must be at least 30 characters long'],
         trim: true,
         uppercase: true
     }
