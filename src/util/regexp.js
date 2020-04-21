62c12e6702f293fa802e94891892c780338232cd
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-20 21:29:48 -0600

    Install xregexp for unicode regex, change fullname and singlename regex

--- src/util/regexp.js
+++ src/util/regexp.js
@@ -1,7 +1,9 @@
+const XRegExp = require('xregexp');
+
 module.exports = {
     roleName: /^ROLE_/,
-    singleName: /^\p{L}+$/,
-    fullName: /^(?:[\p{L}\p{Mn}\p{Pd}\'\x{2019}]+\s[\p{L}\p{Mn}\p{Pd}\'\x{2019}]+\s?)+$/,
+    singleName: XRegExp(/^\p{L}+$/),
+    fullName: XRegExp(/^(?:[\p{L}\p{Mn}\p{Pd}\'\x{2019}]+\s[\p{L}\p{Mn}\p{Pd}\'\x{2019}]+\s?)+$/),
     username: /^(?![_.\d])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
     mimeType: /\w+\/[-+.\w]+/,
     filename: /^[\w,\s-\._]+\.[A-Za-z]{1,}$/
