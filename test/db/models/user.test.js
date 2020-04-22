83c38b4d91f84a0ff0ffc501be9e0100c762fc08
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-22 10:04:21 -0600

    Refactor user tests to use fixture function model validators

--- test/db/models/user.test.js
+++ test/db/models/user.test.js
@@ -2,6 +2,8 @@ const User = require('../../../src/db/models/user');
 
 const { userDefinition } = require('../../../src/db/schemas/user');
 
+const modelFunctions = require('../../__fixtures__/functions/models');
+
 let userDoc;
 let user;
 
@@ -32,80 +34,38 @@ describe('Invalid user names', () => {
     const [userMaxLength] = userDefinition.name.maxlength;
 
     test('Should not validate a user without a name', () => {
-
         user.name = undefined;
-
-        const validationError = user.validateSync();
-        const [, validationMessage] = userDefinition.name.required;
-        
-        expect(validationError.message.includes(validationMessage)).toBe(true);
-
+        modelFunctions.testForInvalidModelField(user, userDefinition.name.required);
     });
 
     test('Should not validate a user with a name that does not match the fullName regexp pattern', () => {
-
         user.name = 'Max+ User!';
-    
-        const validationError = user.validateSync();
-        const [, validationMessage] = userDefinition.name.validate;
-
-        expect(validationError.message.includes(validationMessage)).toBe(true);
-
+        modelFunctions.testForInvalidModelField(user, userDefinition.name.validate);
     });
 
     test('Should not validate a user with a name that does not match the singleName regexp pattern', () => {
-
         user.name = '.Peter';
-    
-        const validationError = user.validateSync();
-        const [, validationMessage] = userDefinition.name.validate;
-
-        expect(validationError.message.includes(validationMessage)).toBe(true);
-
+        modelFunctions.testForInvalidModelField(user, userDefinition.name.validate);
     });
 
     test('Should not validate a user with an explicit profane name', () => {
-
         user.name = 'Pussy Destroyer';
-
-        const validationError = user.validateSync();
-        const [, validationMessage] = userDefinition.name.validate;
-    
-        expect(validationError.message.includes(validationMessage)).toBe(true);
-
+        modelFunctions.testForInvalidModelField(user, userDefinition.name.validate);
     });
 
     test('Should not validate a user with an implicit profane name', () => {
-
         user.name = 'Matthew Cumblast';
-
-        const validationError = user.validateSync();
-        const [, validationMessage] = userDefinition.name.validate;
-    
-        expect(validationError.message.includes(validationMessage)).toBe(true);
-
+        modelFunctions.testForInvalidModelField(user, userDefinition.name.validate);
     });
 
     test(`Should not validate a user with a name shorter than ${ userMinLength } characters`, () => {
-
         user.name = 'A';
-
-        const validationError = user.validateSync();
-        const [, validationMessage] = userDefinition.name.minlength;
-
-        expect(validationError.message.includes(validationMessage)).toBe(true);
-
+        modelFunctions.testForInvalidModelField(user, userDefinition.name.minlength);
     });
 
     test(`Should not validate a user with a name longer than ${ userMaxLength } characters`, () => {
-
         user.name = 'Hey this is a reaaaaaaallllyyy looooong super name that should not enter into the database ever since its pretty long you now';
-
-        const validationError = user.validateSync();
-        const [, validationMessage] = userDefinition.name.maxlength;
-
-        expect(validationError.message.includes(validationMessage)).toBe(true);
-
+        modelFunctions.testForInvalidModelField(user, userDefinition.name.maxlength);
     });
 
 });
@@ -113,21 +73,15 @@ describe('Invalid user names', () => {
 describe('Valid user names', () => {
 
     test('Should validate a correct user name', () => {
-
         user.name = 'Brian O\' Connor';
-
-        const validationError = user.validateSync();
-        expect(validationError).not.toBeDefined();
-
+        modelFunctions.testForValidModelField(user);
     });
 
     test('Should trim a valid user name', () => {
 
         user.name = '   John        Smith    ';
+        modelFunctions.testForValidModelField(user);
 
-        const validationError = user.validateSync();
-
-        expect(validationError).not.toBeDefined();
         expect(user.name).toBe('John Smith');
 
     });
@@ -140,58 +94,28 @@ describe('Invalid user usernames', () => {
     const [usernameMaxLength] = userDefinition.username.maxlength;
 
     test('Should not validate a user without a username', () => {
-
         user.username = undefined;
-
-        const validationError = user.validateSync();
-        const [, validationMessage] = userDefinition.username.required;
-
-        expect(validationError.message.includes(validationMessage)).toBe(true);
-
+        modelFunctions.testForInvalidModelField(user, userDefinition.username.required);
     });
 
     test('Should not validate a user with a username that does not match the username regexp pattern', () => {
-
         user.username = 'max+!';
-    
-        const validationError = user.validateSync();
-        const [, validationMessage] = userDefinition.username.validate;
-
-        expect(validationError.message.includes(validationMessage)).toBe(true);
-
+        modelFunctions.testForInvalidModelField(user, userDefinition.username.validate);
     });
 
     test('Should not validate a user with a profane username', () => {
-
         user.username = 'colossalanus';
-    
-        const validationError = user.validateSync();
-        const [, validationMessage] = userDefinition.username.validate;
-
-        expect(validationError.message.includes(validationMessage)).toBe(true);
-
+        modelFunctions.testForInvalidModelField(user, userDefinition.username.validate);
     });
 
     test(`Should not validate a user with a username shorter than ${ usernameMinLength } characters`, () => {
-
-        user.username = 'cz'
-
-        const validationError = user.validateSync();
-        const [, validationMessage] = userDefinition.username.minlength;
-
-        expect(validationError.message.includes(validationMessage)).toBe(true);
-
+        user.username = 'cz';
+        modelFunctions.testForInvalidModelField(user, userDefinition.username.minlength);
     });
 
     test(`Should not validate a user with a username longer than ${ usernameMaxLength } characters`, () => {
-
-        user.username = 'superlongusernamebruh'
-
-        const validationError = user.validateSync();
-        const [, validationMessage] = userDefinition.username.maxlength;
-
-        expect(validationError.message.includes(validationMessage)).toBe(true);
-
+        user.username = 'superlongusernamebruh';
+        modelFunctions.testForInvalidModelField(user, userDefinition.username.maxlength);
     });
 
 });
@@ -199,21 +123,15 @@ describe('Invalid user usernames', () => {
 describe('Valid user usernames', () => {
 
     test('Should validate a correct username', () => {
-
-        user.username = 'lion_delta42'
-
-        const validationError = user.validateSync();
-        expect(validationError).not.toBeDefined();
-
+        user.username = 'lion_delta42';
+        modelFunctions.testForValidModelField(user);
     });
 
     test('Should trim a valid username', () => {
 
-        user.username = '   magician.red_68     '
+        user.username = '   magician.red_68     ';
+        modelFunctions.testForValidModelField(user);
 
-        const validationError = user.validateSync();
-
-        expect(validationError).not.toBeDefined();
         expect(user.username).toBe('magician.red_68');
 
     });
@@ -223,25 +141,13 @@ describe('Valid user usernames', () => {
 describe('Invalid user emails', () => {
 
     test('Should not validate a user without an email', () => {
-
         user.email = undefined;
-
-        const validationError = user.validateSync();
-        const [, validationMessage] = userDefinition.email.required;
-
-        expect(validationError.message.includes(validationMessage)).toBe(true);
-
+        modelFunctions.testForInvalidModelField(user, userDefinition.email.required);
     });
 
     test('Should not validate a user with an invalid email', () => {
-
         user.email = 'this-is@not-an@email.com';
-    
-        const validationError = user.validateSync();
-        const [, validationMessage] = userDefinition.email.validate;
-    
-        expect(validationError.message.includes(validationMessage)).toBe(true);
-
+        modelFunctions.testForInvalidModelField(user, userDefinition.email.validate);
     });
 
 });
@@ -249,21 +155,15 @@ describe('Invalid user emails', () => {
 describe('Valid user emails', () => {
 
     test('Should validate a correct email', () => {
-
-        user.email = 'john_smith@hotmail.com'
-
-        const validationError = user.validateSync();
-        expect(validationError).not.toBeDefined();
-
+        user.email = 'john_smith@hotmail.com';
+        modelFunctions.testForValidModelField(user);
     });
 
     test('Should trim a valid email', () => {
 
-        user.email = '   smith@something.org     '
+        user.email = '   smith@something.org     ';
+        modelFunctions.testForValidModelField(user);
 
-        const validationError = user.validateSync();
-
-        expect(validationError).not.toBeDefined();
         expect(user.email).toBe('smith@something.org');
 
     });
@@ -276,36 +176,18 @@ describe('Invalid user passwords', () => {
     const [passwordMaxLength] = userDefinition.password.maxlength;
 
     test('Should not validate a user without a password', () => {
-
         user.password = undefined;
-
-        const validationError = user.validateSync();
-        const [, validationMessage] = userDefinition.password.required;
-
-        expect(validationError.message.includes(validationMessage)).toBe(true);
-
+        modelFunctions.testForInvalidModelField(user, userDefinition.password.required);
     });
 
     test(`Should not validate a user with a password shorter than ${ passwordMinLength } characters (unhashed)`, () => {
-
         user.password = 'this-is-an-unhashed-password';
-
-        const validationError = user.validateSync();
-        const [, validationMessage] = userDefinition.password.minlength;
-
-        expect(validationError.message.includes(validationMessage)).toBe(true);
-
+        modelFunctions.testForInvalidModelField(user, userDefinition.password.minlength);
     });
 
     test(`Should not validate a user with a password longer than ${ passwordMaxLength } characters (unhashed)`, () => {
-
         user.password = 'really-long-password-that-is-not-actually-hashed-with-bcrypt-so-it-should-fail';
-
-        const validationError = user.validateSync();
-        const [, validationMessage] = userDefinition.password.maxlength;
-
-        expect(validationError.message.includes(validationMessage)).toBe(true);
-
+        modelFunctions.testForInvalidModelField(user, userDefinition.password.maxlength);
     });
 
 });
@@ -313,12 +195,8 @@ describe('Invalid user passwords', () => {
 describe('Valid user passwords', () => {
 
     test('Should validate a correct password', () => {
-
-        user.password = '$2y$12$r2ey63ZhWsufGBHhnK8Y4uAxUQrGGdxOwETEa7NtVdZJCyWn6yrnW'
-
-        const validationError = user.validateSync();
-        expect(validationError).not.toBeDefined();
-
+        user.password = '$2y$12$r2ey63ZhWsufGBHhnK8Y4uAxUQrGGdxOwETEa7NtVdZJCyWn6yrnW';
+        modelFunctions.testForValidModelField(user);
     });
 
 });
@@ -326,17 +204,12 @@ describe('Valid user passwords', () => {
 describe('User avatar', () => {
 
     test('Should validate a correctly defined file schema', () => {
-        const validationError = user.validateSync();
-        expect(validationError).not.toBeDefined();
+        modelFunctions.testForValidModelField(user);
     });
 
     test('Should validate a user without a defined file schema', () => {
-
         user.avatar = undefined;
-
-        const validationError = user.validateSync();
-        expect(validationError).not.toBeDefined();
-
+        modelFunctions.testForValidModelField(user);
     });
 
 });
