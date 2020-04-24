042520d4852dd0fa841a4b4ea774496e4defae49
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-24 13:55:46 -0600

    New int. tests on user file keynames and hashed passwords

--- test/__integration__/db/models/user.test.js
+++ test/__integration__/db/models/user.test.js
@@ -4,6 +4,8 @@ const { mongo } = require('mongoose');
 
 const User = require('../../../../src/db/models/user');
 const userContexts = require('../../../__fixtures__/functions/db/models/user');
+
+const { sampleUser } = require('../../../__fixtures__/models/user/persisted');
 const { nonUniqueUsers, uniqueUsers } = require('../../../__fixtures__/models/user/unpersisted');
 
 const expect = chai.expect;
@@ -41,8 +43,14 @@ describe('[db/models/user] - Non-unique emails', () => {
 describe('[db/models/user] - Non-unique avatar keynames', () => {
 
     it('Should not persist a user with a non-unique avatar keyname', async () => {
-        const duplicateUser = new User(nonUniqueUsers.nonUniqueAvatarKeyname);
-        await expect(duplicateUser.save()).to.eventually.be.rejectedWith(mongo.MongoError);
+
+        const persistedUser = await User.findOne({ _id: sampleUser._id });
+
+        const unpersistedUser = new User(uniqueUsers[0]);
+        unpersistedUser.avatar.keyname = persistedUser.avatar.keyname;
+
+        await expect(unpersistedUser.save()).to.eventually.be.rejectedWith(mongo.MongoError);
+
     });
 
 });
@@ -52,6 +60,33 @@ describe('[db/models/user] - Unique username, email and avatar keyname', () => {
     it('Should persist a user with a required unique fields', async () => {
         const user = new User(uniqueUsers[0]);
         await expect(user.save()).to.eventually.be.eql(user);
+    });
+
+});
+
+describe('[db/models/user] - Pre validate file hook', () => {
+
+    it('Should not regenerate a new file keyname upon user model update', async () => {
+
+        const user = await User.findOne({ _id: sampleUser._id });
+        const oldAvatarKeyname = user.avatar.keyname;
+
+        const updatedUser = await user.save();
+        expect(updatedUser.avatar.keyname).to.equal(oldAvatarKeyname);
+
+    });
+
+});
+
+describe('[db/models/user] - Pre save hook', () => {
+
+    it('Should persist a user with a hashed password', async () => {
+
+        const user = await User.findOne({ _id: sampleUser._id });
+
+        expect(user.password.length).to.equal(60);
+        expect(user.password).to.not.equal(sampleUser.password);
+
     });
 
 });
