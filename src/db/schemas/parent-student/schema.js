f9a1e0edfc822d27d643513048d8aaa46fb0ae39
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-27 10:33:17 -0600

    Add parentSchema static method to add a record with validation

--- src/db/schemas/parent-student/schema.js
+++ src/db/schemas/parent-student/schema.js
@@ -1,9 +1,9 @@
-const { Schema } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const parentStudentDefinition = require('./definition');
 const { parentStudent, user } = require('../../names');
 
-const roles = require('../../../util/roles');
+const roleTypes = require('../../../util/roles');
 
 const schemaOpts = {
     collection: parentStudent.collectionName
@@ -12,5 +12,39 @@ const schemaOpts = {
 const parentStudentSchema = new Schema(parentStudentDefinition, schemaOpts);
 
 parentStudentSchema.index({ parent: 1, student: 1 }, { unique: true });
+
+parentStudentSchema.statics.add = async function(parentStudentDoc) {
+
+    const ParentStudent = this;
+    const User = model(user.modelName);
+
+    const { parent: parentId } = parentStudentDoc;
+
+    const parent = await User.findOne({
+        _id: parentId,
+        enabled: true
+    });
+
+    if (!parent) {
+        throw new Error('The parent deleted/disabled his/her account');
+    }
+
+    const isParent = await parent.hasRole(roleTypes.ROLE_PARENT);
+
+    if (!isParent) {
+        throw new Error('The user is not a parent');
+    }
+
+    const parentStudent = new ParentStudent(parentStudentDoc);
+
+    try {
+        await parentStudent.save();
+    } catch (e) {
+        throw e;
+    }
+
+    return parentStudent;
+
+};
 
 module.exports = parentStudentSchema;
