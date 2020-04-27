7a56bc51da6a15752cfb3e28f7d013f4cb52f567
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-27 09:07:33 -0600

    Delete pre save hook of parent-student schema

--- src/db/schemas/parent-student/schema.js
+++ src/db/schemas/parent-student/schema.js
@@ -13,22 +13,4 @@ const parentStudentSchema = new Schema(parentStudentDefinition, schemaOpts);
 
 parentStudentSchema.index({ parent: 1, student: 1 }, { unique: true });
 
-parentStudentSchema.pre('save', async function(next) {
-
-    const parentStudent = this;
-
-    const parentUser = await parentStudent.model(user.modelName).findOne({ _id: parentStudent.parent });
-    const studentUser = await parentStudent.model(user.modelName).findOne({ _id: parentStudent.student });
-
-    const parentRoles = await parentUser.getUserRoles();
-    const studentRoles = await studentUser.getUserRoles();
-
-    if (parentRoles.includes(roles.ROLE_PARENT) && studentRoles.includes(roles.ROLE_STUDENT)) {
-        next();
-    } else {
-        next(new Error('Role mismatch'));
-    }
-
-});
-
 module.exports = parentStudentSchema;
