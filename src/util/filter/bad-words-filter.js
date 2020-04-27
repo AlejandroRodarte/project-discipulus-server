64169c1ce994a477c57be748dc1eaf944b926fbb
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-26 23:09:32 -0600

    Add whitelist words

--- src/util/filter/bad-words-filter.js
+++ src/util/filter/bad-words-filter.js
@@ -2,6 +2,8 @@ const Filter = require('bad-words');
 
 const badWordsFilter = new Filter();
 
+badWordsFilter.removeWords('Gaylord', 'Willy', 'Fanny', 'sexy', 'pula')
+
 const includesProfaneWord = word => badWordsFilter.list.some(badWord => word.toLowerCase().includes(badWord.toLowerCase()));
 
 module.exports = {
