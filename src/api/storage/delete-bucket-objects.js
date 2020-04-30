df4ede2abd4d9017d196ad5ab1a206341c932d03
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-30 16:45:54 -0600

    Add api method to delete bucket objects

---
+++ src/api/storage/delete-bucket-objects.js
@@ -0,0 +1,25 @@
+const cos = require('./config/cos');
+
+const deleteBucketObjects = (bucketName, keynames) => {
+
+    const objects = keynames.map(keyname => ({ Key: keyname }));
+
+    try {
+
+        const data = await cos.deleteObjects({
+            Bucket: bucketName,
+            Delete: {
+                Objects: objects
+            }
+        }).promise();
+
+        return data.Deleted;
+
+    } catch (e) {
+        throw e;
+    }
+
+
+};
+
+module.exports = deleteBucketObjects;
--- src/api/storage/index.js
+++ src/api/storage/index.js
@@ -1,9 +1,11 @@
 const createMultipartObject = require('./create-multipart-object');
 const getMultipartObject = require('./get-multipart-object');
 const listBucketKeys = require('./list-bucket-keys');
+const deleteBucketObjects = require('./delete-bucket-objects');
 
 module.exports = {
     createMultipartObject,
     getMultipartObject,
-    listBucketKeys
+    listBucketKeys,
+    deleteBucketObjects
 };
