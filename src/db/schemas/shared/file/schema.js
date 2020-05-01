faa92150dff618a4deb4f019ddc263a1a804be91
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-30 18:42:01 -0600

    Uninstall uuid

--- src/db/schemas/shared/file/schema.js
+++ src/db/schemas/shared/file/schema.js
@@ -1,10 +1,7 @@
 const { Schema } = require('mongoose');
-const { v4 } = require('uuid');
 
 const sharedFileDefinition = require('./definition');
 const { sharedFile } = require('../../../names');
-
-const uuidv4 = v4;
 
 const schemaOpts = {
     collection: sharedFile.collectionName
