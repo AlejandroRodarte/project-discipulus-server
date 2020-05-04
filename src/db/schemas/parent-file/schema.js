626a39a6a5e907e35f15fc0aac518175305ba72a
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-05-04 12:11:21 -0600

    Add ParentFile index, pre remove and methods.saveFileAndDoc logic

--- src/db/schemas/parent-file/schema.js
+++ src/db/schemas/parent-file/schema.js
@@ -1,12 +1,76 @@
 const { Schema } = require('mongoose');
 
 const parentFileDefinition = require('./definition');
-const { parentFile } = require('../../names');
+const { user, parentFile } = require('../../names');
+
+const storageApi = require('../../../api/storage');
+const bucketNames = require('../../../api/storage/config/bucket-names');
+
+const roleTypes = require('../../../util/roles');
 
 const schemaOpts = {
     collection: parentFile.collectionName
 };
 
-const parentStudentSchema = new Schema(parentFileDefinition, schemaOpts);
+const parentFileSchema = new Schema(parentFileDefinition, schemaOpts);
 
-module.exports = parentStudentSchema;
+parentFileSchema.index({ user: 1, 'file.originalname': 1 }, { unique: true });
+
+parentFileSchema.pre('remove', async function(next) {
+
+    const parentFileDoc = this;
+
+    try {
+        await storageApi.deleteBucketObjects(bucketNames[parentFile.modelName], [parentFileDoc.file.keyname]);
+        next();
+    } catch (e) {
+        next(new Error('File could not be deleted'));
+    }
+
+});
+
+parentFileSchema.methods.saveFileAndDoc = async function(buffer) {
+
+    const parentFileDoc = this;
+    const User = parentFileDoc.model(user.modelName);
+
+    const { user: userId } = parentFileDoc;
+
+    const parentDoc = await User.findOne({ 
+        _id: userId,
+        enabled: true
+    });
+
+    if (!parentDoc) {
+        throw new Error('Your account is currently disabled or has been deleted');
+    }
+
+    const isParent = await parentDoc.hasRole(roleTypes.ROLE_PARENT);
+
+    if (!isParent) {
+        throw new Error('You must be a parent in order to save a file here');
+    }
+
+    try {
+        await parentFileDoc.save();
+    } catch (e) {
+        throw e;
+    }
+
+    try {
+        await storageApi.createMultipartObject(bucketNames[parentFile.modelName], {
+            keyname: parentFileDoc.file.keyname,
+            buffer,
+            size: buffer.length,
+            mimetype: parentFileDoc.file.mimetype
+        });
+    } catch (e) {
+        await parentFileDoc.remove();
+        throw e;
+    }
+
+    return parentFileDoc;
+
+};
+
+module.exports = parentFileSchema;
