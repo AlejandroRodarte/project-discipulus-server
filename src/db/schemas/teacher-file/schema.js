637c072c727f0946815832b8ed5268880850c824
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-05-04 21:09:13 -0600

    Add index/pre remove hook/saveFileAndDoc custom functions for StudentFile and TeacherFile

--- src/db/schemas/teacher-file/schema.js
+++ src/db/schemas/teacher-file/schema.js
@@ -3,10 +3,28 @@ const { Schema } = require('mongoose');
 const teacherFileDefinition = require('./definition');
 const { teacherFile } = require('../../names');
 
+const { generatePreRemoveHook, generateSaveFileAndDocMethod } = require('../../../util/models/user-file');
+
+const roleTypes = require('../../../util/roles');
+
 const schemaOpts = {
     collection: teacherFile.collectionName
 };
 
 const teacherFileSchema = new Schema(teacherFileDefinition, schemaOpts);
+
+teacherFile.index({ user: 1, 'file.originalname': 1 }, { unique: true });
+
+teacherFile.pre('remove', generatePreRemoveHook({
+    modelName: teacherFile.modelName
+}));
+
+teacherFile.methods.saveFileAndDoc = generateSaveFileAndDocMethod({
+    modelName: teacherFile.modelName,
+    roleOpts: {
+        check: true,
+        role: roleTypes.ROLE_TEACHER
+    }
+});
 
 module.exports = teacherFileSchema;
