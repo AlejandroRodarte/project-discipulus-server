205eb94dc00f06dca14d8b48cd35994fd03c8d80
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-05-10 21:26:59 -0600

    Add classSchema.methods.checkAndSave

--- src/db/schemas/class/schema.js
+++ src/db/schemas/class/schema.js
@@ -1,14 +1,46 @@
-const { Schema } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const classDefinition = require('./definition');
-const { class: clazz } = require('../../names');
+const names = require('../../names');
+
+const roleTypes = require('../../../util/roles');
 
 const schemaOpts = {
-    collection: clazz.collectionName
+    collection: names.class.collectionName
 };
 
 const classSchema = new Schema(classDefinition, schemaOpts);
 
 classSchema.index({ user: 1, title: 1 }, { unique: true });
+
+classSchema.methods.checkAndSave = function() {
+
+    const clazz = this;
+    const User = model(names.user.modelName);
+
+    const teacher = await User.findOne({
+        _id: clazz.user,
+        enabled: true
+    });
+
+    if (!teacher) {
+        throw new Error('The user has its account deleted/disabled');
+    }
+
+    const isTeacher = await teacher.hasRole(roleTypes.ROLE_TEACHER);
+
+    if (!isTeacher) {
+        throw new Error('The owner for this class should be a teacher');
+    }
+
+    try {
+        await clazz.save();
+    } catch (e) {
+        throw e;
+    }
+
+    return clazz;
+
+};
 
 module.exports = classSchema;
