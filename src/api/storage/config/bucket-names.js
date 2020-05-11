560099ff2129295cc24f313c75189699df7d6e95
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-05-10 22:15:32 -0600

    Add src code to classSchema.methods.saveAvatar:

--- src/api/storage/config/bucket-names.js
+++ src/api/storage/config/bucket-names.js
@@ -6,5 +6,6 @@ module.exports = {
     [names.userFile.modelName]: `${process.env.BUCKET_PREFIX_NAME}-user-files`,
     [names.teacherFile.modelName]: `${process.env.BUCKET_PREFIX_NAME}-teacher-files`,
     [names.studentFile.modelName]: `${process.env.BUCKET_PREFIX_NAME}-student-files`,
-    [names.parentFile.modelName]: `${process.env.BUCKET_PREFIX_NAME}-parent-files`
+    [names.parentFile.modelName]: `${process.env.BUCKET_PREFIX_NAME}-parent-files`,
+    [names.class.modelName]: `${process.env.BUCKET_PREFIX_NAME}-class-files`
 };
--- src/db/schemas/class/schema.js
+++ src/db/schemas/class/schema.js
@@ -5,6 +5,9 @@ const names = require('../../names');
 
 const roleTypes = require('../../../util/roles');
 
+const storageApi = require('../../../api/storage');
+const bucketNames = require('../../../api/storage/config/bucket-names');
+
 const schemaOpts = {
     collection: names.class.collectionName
 };
@@ -37,6 +40,48 @@ classSchema.methods.checkAndSave = async function() {
         await clazz.save();
     } catch (e) {
         throw e;
+    }
+
+    return clazz;
+
+};
+
+classSchema.methods.saveAvatar = async function(avatarDoc, buffer) {
+
+    const clazz = this;
+
+    if (clazz.avatar) {
+
+        try {
+            await storageApi.deleteBucketObjects(bucketNames[names.class.modelName], [clazz.avatar.keyname]);
+        } catch (e) {
+            throw e;
+        }
+
+    }
+
+    clazz.avatar = avatarDoc;
+
+    try {
+        await clazz.save();
+    } catch (e) {
+        throw e;
+    }
+
+    try {
+        await storageApi.createMultipartObject(bucketNames[names.class.modelName], {
+            keyname: clazz.avatar.keyname,
+            buffer,
+            size: buffer.length,
+            mimetype: clazz.avatar.mimetype
+        });
+    } catch (e) {
+
+        clazz.avatar = undefined;
+        await clazz.save();
+
+        throw e;
+
     }
 
     return clazz;
