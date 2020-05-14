f12831f75327db60256af9900746cecdfe5708d5
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-05-14 10:25:15 -0600

    Add unit tests to ClassUnknownStudentInvitation methods.checkAndSave

--- test/__unit__/db/models/class-unknown-student-invitation.test.js
+++ test/__unit__/db/models/class-unknown-student-invitation.test.js
@@ -1,20 +1,38 @@
 const { Types } = require('mongoose');
 const faker = require('faker');
 
-const expect = require('chai').expect;
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const sinon = require('sinon');
 
-const { ClassUnknownStudentInvitation } = require('../../../../src/db/models');
+const { ClassUnknownStudentInvitation, ClassStudentInvitation, User } = require('../../../../src/db/models');
 const { classUnknownStudentInvitationDefinition } = require('../../../../src/db/schemas/class-unknown-student-invitation');
 const modelFunctions = require('../../../__fixtures__/functions/models');
+
+const expect = chai.expect;
+chai.use(chaiAsPromised);
 
 const classUnknownStudentInvitationDoc = {
     class: new Types.ObjectId(),
     email: faker.internet.email()
 };
 
-let classUnknownStudentInvitation = new ClassUnknownStudentInvitation(classUnknownStudentInvitationDoc);
+const classStudentInvitationDoc = {
+    class: new Types.ObjectId(),
+    user: new Types.ObjectId()
+};
 
-beforeEach(() => classUnknownStudentInvitation = modelFunctions.getNewModelInstance(ClassUnknownStudentInvitation, classUnknownStudentInvitationDoc));
+const [studentDoc] = modelFunctions.generateFakeUsers(1, { fakeToken: true });
+
+let classUnknownStudentInvitation = new ClassUnknownStudentInvitation(classUnknownStudentInvitationDoc);
+let classStudentInvitation = new ClassStudentInvitation(classStudentInvitationDoc);
+let student = new User(studentDoc);
+
+beforeEach(() => {
+    classUnknownStudentInvitation = modelFunctions.getNewModelInstance(ClassUnknownStudentInvitation, classUnknownStudentInvitationDoc);
+    classStudentInvitation = modelFunctions.getNewModelInstance(ClassStudentInvitation, classStudentInvitationDoc);
+    student = modelFunctions.getNewModelInstance(User, studentDoc);
+});
 
 describe('[db/models/class-unknown-student-invitation] - Invalid class', () => {
 
@@ -58,5 +76,64 @@ describe('[db/models/class-unknown-student-invitation] - Valid model', () => {
     it('Should validate correct class-unknown-student invitation model', () => {
         modelFunctions.testForValidModel(classUnknownStudentInvitation);
     })
+
+});
+
+describe('[db/models/class-unknown-student-invitation] - methods.checkAndSave', () => {
+
+    let userFindOneStub;
+    let classStudentInvitationCheckAndSaveStub;
+    let classUnknownStudentInvitationSaveStub;
+
+    it('Should throw error if User.findOne (called with correct args) finds a user, a regular invitation is made but checkAndSave fails', async () => {
+
+        userFindOneStub = sinon.stub(User, 'findOne').resolves(student);
+        classStudentInvitationCheckAndSaveStub = sinon.stub(ClassStudentInvitation.prototype, 'checkAndSave').rejects();
+
+        await expect(classUnknownStudentInvitation.checkAndSave()).to.eventually.be.rejectedWith(Error);
+
+        sinon.assert.calledOnceWithExactly(userFindOneStub, {
+            email: classUnknownStudentInvitation.email
+        });
+
+        sinon.assert.calledOnce(classStudentInvitationCheckAndSaveStub);
+
+    });
+
+    it('Should return class-student invitation instance if regular invitation checkAndSave resolves', async () => {
+
+        userFindOneStub = sinon.stub(User, 'findOne').resolves(student);
+        classStudentInvitationCheckAndSaveStub = sinon.stub(ClassStudentInvitation.prototype, 'checkAndSave').resolves(classStudentInvitation);
+
+        await expect(classUnknownStudentInvitation.checkAndSave()).to.eventually.include({
+            class: classUnknownStudentInvitation.class,
+            user: student._id
+        });
+
+    });
+
+    it('Should throw error if classUnknownStudentInvitation.save fails', async () => {
+
+        userFindOneStub = sinon.stub(User, 'findOne').resolves(null);
+        classUnknownStudentInvitationSaveStub = sinon.stub(classUnknownStudentInvitation, 'save').rejects();
+
+        await expect(classUnknownStudentInvitation.checkAndSave()).to.eventually.be.rejectedWith(Error);
+
+        sinon.assert.calledOnce(classUnknownStudentInvitationSaveStub);
+
+    });
+
+    it('Should return class-unknown-student invitation on success', async () => {
+
+        userFindOneStub = sinon.stub(User, 'findOne').resolves(null);
+        classUnknownStudentInvitationSaveStub = sinon.stub(classUnknownStudentInvitation, 'save').resolves(classUnknownStudentInvitation);
+
+        await expect(classUnknownStudentInvitation.checkAndSave()).to.eventually.eql(classUnknownStudentInvitation);
+
+    });
+
+    afterEach(() => {
+        sinon.restore();
+    });
 
 });
