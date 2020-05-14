2605f3d4072888467e0a7192b627b8ac07065f21
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-05-13 23:48:05 -0600

    Add classUnknownStudentInvitationSchema.methods.checkAndSave to ClassUnknownStudentInvitation

--- src/db/schemas/class-unknown-student-invitation/schema.js
+++ src/db/schemas/class-unknown-student-invitation/schema.js
@@ -1,14 +1,52 @@
 const { Schema } = require('mongoose');
 
 const classUnknownStudentInvitationDefinition = require('./definition');
-const { classUnknownStudentInvitation } = require('../../names');
+const names = require('../../names');
 
 const schemaOpts = {
-    collection: classUnknownStudentInvitation.collectionName
+    collection: names.classUnknownStudentInvitation.collectionName
 };
 
 const classUnknownStudentInvitationSchema = new Schema(classUnknownStudentInvitationDefinition, schemaOpts);
 
 classUnknownStudentInvitationSchema.index({ class: 1, email: 1 }, { unique: true });
+
+classUnknownStudentInvitationSchema.methods.checkAndSave = async function() {
+
+    const classUnknownStudentInvitation = this;
+
+    const User = classUnknownStudentInvitation.model(names.user.modelName);
+    const ClassStudentInvitation = classUnknownStudentInvitation.model(names.classStudentInvitation.modelName);
+
+    const student = await User.findOne({
+        email: classUnknownStudentInvitation.email
+    });
+
+    if (student) {
+
+        const classStudentInvitation = new ClassStudentInvitation({
+            class: classUnknownStudentInvitation.class,
+            user: student._id
+        });
+
+        try {
+            await classStudentInvitation.checkAndSave();
+        } catch (e) {
+            throw e;
+        }
+
+        return classStudentInvitation;
+
+    }
+
+    try {
+        await classUnknownStudentInvitation.save();
+    } catch (e) {
+        throw e;
+    }
+
+    return classUnknownStudentInvitation;
+
+};
 
 module.exports = classUnknownStudentInvitationSchema;
