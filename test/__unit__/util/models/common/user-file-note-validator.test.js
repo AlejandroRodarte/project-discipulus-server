aea71ec07a3b9935f29bd8149bf358ddc636c0e9
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-05-15 20:49:08 -0600

    Unit tests for userFileNoteValidator

---
+++ test/__unit__/util/models/common/user-file-note-validator.test.js
@@ -0,0 +1,54 @@
+const { Types } = require('mongoose');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const sinon = require('sinon');
+
+const { userFileNoteValidator } = require('../../../../../src/util/models/common');
+const { generateFakeNote, getNewModelInstance } = require('../../../../__fixtures__/functions/models');
+
+const { User, UserNote } = require('../../../../../src/db/models');
+
+const expect = chai.expect;
+chai.use(chaiAsPromised);
+
+const { modelErrorMessages } = require('../../../../../src/util/errors');
+
+const userNoteDoc = {
+    user: new Types.ObjectId(),
+    note: generateFakeNote({
+        titleWords: 5,
+        descriptionWords: 10,
+        markdown: '# Test'
+    })
+};
+
+let userNote = new UserNote(userNoteDoc);
+
+beforeEach(() => userNote = getNewModelInstance(UserNote, userNoteDoc));
+
+describe('[util/models/common/user-file-note-validator]', () => {
+
+    let userExistsStub;
+
+    it('Should throw error if User.exists (called with correct args) resolves to false', async () => {
+
+        userExistsStub = sinon.stub(User, 'exists').resolves(false);
+        await expect(userFileNoteValidator(userNote)).to.eventually.be.rejectedWith(Error, modelErrorMessages.userNotFoundOrDisabled);
+
+        sinon.assert.calledOnceWithExactly(userExistsStub, {
+            _id: userNote.user,
+            enabled: true
+        });
+
+    });
+
+    it('Should resolve if user does exist', async () => {
+        userExistsStub = sinon.stub(User, 'exists').resolves(true);
+        await expect(userFileNoteValidator(userNote)).to.eventually.be.fulfilled;
+    });
+
+    afterEach(() => {
+        sinon.restore();
+    });
+
+});
