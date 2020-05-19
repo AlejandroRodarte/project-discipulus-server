6d06aa6bc48e6b6f637011b2aefb77e644838c8e
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-05-18 22:36:32 -0600

    Simple unit test to getEnabledClassStudentIds

---
+++ test/__unit__/db/aggregation/class-student/get-enabled-class-student-ids.test.js
@@ -0,0 +1,18 @@
+const { Types } = require('mongoose');
+const expect = require('chai').expect;
+
+const { aggregation } = require('../../../../../src/db');
+
+describe('[db/aggregation/class-student] - getEnabledClassStudentIds', () => {
+
+    it('Should return proper pipeline object', () => {
+
+        const classId = new Types.ObjectId();
+        const [firstStage] = aggregation.classStudentPipelines.getEnabledClassStudentIds(classId);
+
+        expect(firstStage.$match.class).to.equal(classId);
+
+    });
+
+});
+
