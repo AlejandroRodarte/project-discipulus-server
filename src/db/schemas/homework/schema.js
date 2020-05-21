e4eca2a789883dc745566de0e79ec6d1cf79ab7b
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-05-21 11:28:19 -0600

    Add homeworkSchema.methods.saveAndAddStudents

--- src/db/schemas/homework/schema.js
+++ src/db/schemas/homework/schema.js
@@ -1,6 +1,7 @@
 const { Schema } = require('mongoose');
 
 const { db } = require('../../../shared');
+const { models, errors } = require('../../../util');
 
 const homeworkDefinition = require('./definition');
 
@@ -11,5 +12,21 @@ const schemaOpts = {
 const homeworkSchema = new Schema(homeworkDefinition, schemaOpts);
 
 homeworkSchema.index({ class: 1, title: 1 }, { unique: true });
+
+homeworkSchema.methods.saveAndAddStudents = models.common.generateSaveAndAddStudents({
+    parent: {
+        modelName: db.names.class.modelName,
+        ref: 'class',
+        notFoundErrorMessage: errors.modelErrorMessages.classNotFound,
+        getIdsMethodName: 'getEnabledStudentIds'
+    },
+    child: {
+        modelName: db.names.homeworkStudent.modelName,
+        doc: {
+            ref1: 'homework',
+            ref2: 'classStudent'
+        }
+    }
+});
 
 module.exports = homeworkSchema;
